4f23ed2bacdc91dc5d4b2859808e2125edf4c868
Ian Farias Paixão <ian.paixao@cwi.com.br>
2022-05-26 19:31:23 -0300

    add header from initial page

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import logo  from "./img/logo.svg"
+import korra_img from './img/korra.png'
+import appa from './img/appa.svg'
+
+
+export {logo, korra_img, appa }
--- src/ui/pages/initial-screen/initial-screen.tsx
+++ src/ui/pages/initial-screen/initial-screen.tsx
@@ -1,9 +1,15 @@
+import { appa, logo } from '../../../assets'
 import './initial-screen.css'
 
 const InitialScreen = () => {
   return (
     <>
-      <header className="initial--header"></header>
+      <header className="initial--header ">
+        <div className="content-wrapper ">
+          <img src={logo} alt="" />
+          <img src={appa} alt="" />
+        </div>
+      </header>
       <main>
         <h1>tela inicial</h1>
       </main>
